91733b32a754b8979207b085618ae35de20c97d9
checkpoint <akegbeyaletoheeb27@gmail.com>
2023-02-15 17:26:59 +0100

    commit all changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// alert("Welcome to my web page");
+// let visitorName = prompt("What's your name : ")
+//  console.log(visitorName)
+let coolwithit = confirm("by visiting our site we will take a screenshoot of your screen")
+console.log(coolwithit)
+
+// my name is olayinka
+/* i am an accountant 
+jalrjpskfjlgkfslk[hgfslbkvl'[klv[kvk[lv]hslhkhosp;lflk
+hojshojoisutgjldhajhujhofuaiojfgkjpdkldpvapklvapkvppkv
+aljkfdjjgpiuatigjigo[koodflagkoktobgk[okgopklfkaogbagkjl*/
+
